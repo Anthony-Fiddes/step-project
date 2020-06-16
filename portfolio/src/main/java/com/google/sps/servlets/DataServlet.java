c3b7e5fd4e8ec6da632a194234b5599cfcddfb2e
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-06-16 16:25:37 -0400

    Add the functionality to delete comments, and week-4 library changes (#10)
    * Add datastore dependency.
    
    * Change data servlet to store comments to datastore
    
    * Add functionality for loading comments from datastore.
    
    * Add support to load a maximum number of comments.
    
    * Add the ability for the user to choose the number of comments to be displayed.
    
    * Add doPost method on /delete-data to remove all comments.
    
    * Add functionality on the main page to delete all comments.
    
    * Fix error of changing response messages.
    
    * Address @ricazhang and @alfredhgoog's comments.
    
    * Change max to initialize using const.
    
    * Add missing semi-colon
    
    * Add a loading icon for the comments section (#11)
    
    * Add a loading icon to indicate that comments are loading.
    
    * Add license for the loading icon into style.css.
    
    * Fix more instances of double quotes in JS file.
    
    * Add support for translating comments using the Google translate API (#14)
    
    * Add dependency for Google translate API.
    
    * Add support in the HTML to select a language.
    
    * Implement translation feature.
    
    * Put constants in alphabetical order.
    
    * Add error handling for invalid translation options.
    
    * Condense all translation requests to one RPC.
    
    * Add support for users to upload images to their comments using blobstore (#15)
    
    * Add HTML structure for submitting an image with comments.
    
    * Add support for users to upload images to server.
    
    * Add blobstore image to comment entity upon POST.
    
    * Add support for serving comments as JSON objects.
    
    * Add support for rendering the posted images.
    
    * Add newline to the end of style.css
    
    * Address error when /data is posted to from a non-blobstore URL.
    
    * Address @ricazhang and @alfredhgoog's comments.
    
    * Add default behavior so that the site still works when there are no comments.
    
    * Add comments to disambiguate between variables 'comments' and 'messages.'
    
    * Remove boolean usingBlobForm from script.js.

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -14,19 +14,34 @@
 
 package com.google.sps.servlets;
 
+import com.google.appengine.api.blobstore.BlobInfo;
+import com.google.appengine.api.blobstore.BlobInfoFactory;
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.datastore.DatastoreService;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
 import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.SortDirection;
+import com.google.cloud.translate.Translate;
+import com.google.cloud.translate.TranslateException;
+import com.google.cloud.translate.TranslateOptions;
+import com.google.cloud.translate.Translation;
 import com.google.gson.Gson;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.ArrayList;
+import java.util.Map;
 import java.util.List;
 
 /**
@@ -36,9 +51,12 @@ import java.util.List;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private static final String COMMENT = "Comment";
+  private static final String CONTENT = "content";
   private static final String CONTENT_TYPE = "application/json";
+  private static final String IMAGE = "image";
+  private static final String LANGUAGE = "language";
   private static final String MAX_COMMENTS = "max";
-  private static final String COMMENT = "content";
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
@@ -48,40 +66,113 @@ public class DataServlet extends HttpServlet {
       maxComments = Integer.parseInt(maxContent);
     } catch (NumberFormatException e) {
       System.err.println("Could not convert to int: " + maxContent);
-      response.sendError(400, "Invalid request.");
+      response.sendError(400, "Invalid parameter \"max\" in request.");
       return;
     }
+    String language = request.getParameter(LANGUAGE);
     Query query = new Query("Comment").addSort("timestamp", SortDirection.DESCENDING);
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     PreparedQuery results = datastore.prepare(query);
+    List<Comment> comments = new ArrayList<>();
     List<String> messages = new ArrayList<>();
     for (Entity entity : results.asIterable()) {
       if (messages.size() >= maxComments) {
         break;
       }
-      String content = (String) entity.getProperty(COMMENT);
+      String content = (String) entity.getProperty(CONTENT);
+      String imageURL = (String) entity.getProperty(IMAGE);
+      // messages is used to translate all comments in one call
       messages.add(content);
+      // comments pairs each comment with its associated image to be exported as
+      // JSON
+      comments.add(new Comment(content, imageURL));
     }
-    Gson gson = new Gson();
-    String json = gson.toJson(messages);
-    response.setContentType(CONTENT_TYPE);
-    response.getWriter().println(json);
+    try {
+      if (comments.size() > 0) {
+        Translate translateService = TranslateOptions.getDefaultInstance().getService();
+        List<Translation> translations = translateService.translate(messages,
+            Translate.TranslateOption.targetLanguage(language));
+        for (int i = 0; i < comments.size(); i++) {
+          comments.get(i).content = translations.get(i).getTranslatedText();
+        }
+      } else {
+        comments.add(new Comment("No comments yet."));
+      }
+      Gson gson = new Gson();
+      String json = gson.toJson(comments);
+      response.setContentType(CONTENT_TYPE);
+      response.getWriter().println(json);
+    } catch (TranslateException e) {
+      System.err.println(e.getMessage());
+      response.sendError(400, "Invalid translation request.");
+      return;
+    }
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    String content = request.getParameter(COMMENT).trim();
+    String content = request.getParameter(CONTENT).trim();
     long timestamp = System.currentTimeMillis();
     if (content.isEmpty()) {
       System.err.println("Empty comment submitted");
       response.sendError(400, "Empty comment submitted");
       return;
     }
-    Entity commentEntity = new Entity("Comment");
+    Entity commentEntity = new Entity(COMMENT);
     commentEntity.setProperty("content", content);
     commentEntity.setProperty("timestamp", timestamp);
+    String imageURL = getUploadedFileUrl(request);
+    if (imageURL != null) {
+      commentEntity.setProperty("image", imageURL);
+    }
     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
     datastore.put(commentEntity);
     response.sendRedirect("/#comments");
+  }
+
+  /**
+   * Returns a URL that points to the uploaded file, or null if the user didn't
+   * upload a file.
+   */
+  private String getUploadedFileUrl(HttpServletRequest request) {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get(IMAGE);
+
+    // User submitted form without selecting a file, so we can't get a URL. (dev
+    // server)
+    if (blobKeys == null || blobKeys.isEmpty()) {
+      return null;
+    }
+
+    // Our form only contains a single file input, so get the first index.
+    BlobKey blobKey = blobKeys.get(0);
+
+    // User submitted form without selecting a file, so we can't get a URL. (live
+    // server)
+    BlobInfo blobInfo = new BlobInfoFactory().loadBlobInfo(blobKey);
+    if (blobInfo.getSize() == 0) {
+      blobstoreService.delete(blobKey);
+      return null;
+    }
+
+    // We could check the validity of the file here, e.g. to make sure it's an image
+    // file
+    // https://stackoverflow.com/q/10779564/873165
+
+    // Use ImagesService to get a URL that points to the uploaded file.
+    ImagesService imagesService = ImagesServiceFactory.getImagesService();
+    ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+
+    // To support running in Google Cloud Shell with AppEngine's devserver, we must
+    // use the relative
+    // path to the image, rather than the path returned by imagesService which
+    // contains a host.
+    try {
+      URL url = new URL(imagesService.getServingUrl(options));
+      return url.getPath();
+    } catch (MalformedURLException e) {
+      return imagesService.getServingUrl(options);
+    }
   }
 }
--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,17 +12,47 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+function onPageLoad() {
+  useBlobForm();
+  getComments();
+}
+
+async function useBlobForm() {
+  const blobResponse = await fetch('/blobstore-upload-url');
+  const blobURL = await blobResponse.text();
+  const form = document.getElementById('comments-form');
+  form.setAttribute('action', blobURL);
+  form.setAttribute('enctype', 'multipart/form-data');
+  form.classList.remove('hidden');
+}
+
 /**
  * Adds comments from /data
  */
 async function getComments() {
-  const commentsData = await fetch('/data?max=5');
+  const max = document.getElementById('max').value;
+  const language = document.getElementById('language').value;
+  const commentsData = await fetch('/data?max=' + max + '&language=' + language);
   const comments = await commentsData.json();
   const ul = document.createElement('ul');
   for (const comment of comments) {
     const li = document.createElement('li');
-    li.innerText = comment;
+    li.innerText = comment.content;
+    if (comment.imageURL) {
+      li.appendChild(document.createElement('br'));
+      const img = document.createElement('img');
+      img.setAttribute('src', comment.imageURL);
+      li.appendChild(img);
+    }
     ul.appendChild(li);
   }
+  document.getElementById('comments-container').innerHTML = '';
   document.getElementById('comments-container').appendChild(ul);
+}
+
+async function deleteComments() {
+  await fetch('/delete-data', {
+    method: 'POST',
+  });
+  getComments();
 }
