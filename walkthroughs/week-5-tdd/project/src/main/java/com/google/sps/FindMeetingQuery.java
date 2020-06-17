fb2e913b1c33b922c7dd60cc7df935bc40f7e8f0
Anthony Fiddes <11233666+Anthony-Fiddes@users.noreply.github.com>
2020-06-17 11:07:21 -0400

    Implement query function as part of week 5 walkthrough.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,103 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.List;
+import java.util.Set;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+
+  /**
+   * Finds available time ranges for all requested attendees to attend the
+   * supplied MeetingRequest, given known events
+   * 
+   * @param events  All known events that will be considered
+   * @param request The meeting request whose attendees must all be free
+   * @return A collection of time ranges where all proposed attendees in @request
+   *         are not attending any known events in @events
+   */
+  public Collection<TimeRange> query(final Collection<Event> events, final MeetingRequest request) {
+    Collection<TimeRange> queryRanges = new ArrayList<>();
+    queryRanges.add(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true));
+    Map<Event, Map<MeetingRequest, Boolean>> cache = new HashMap<>();
+    for (Event event : events) {
+      Collection<TimeRange> nextRanges = new ArrayList<>();
+      for (TimeRange range : queryRanges) {
+        if (hasOverlappingAttendees(event, request, cache)) {
+          nextRanges.addAll(splitToAvailableTimeRanges(range, event.getWhen()));
+        } else {
+          nextRanges.add(range);
+        }
+      }
+      queryRanges = nextRanges;
+    }
+    List<TimeRange> resultRanges = new ArrayList<>();
+    for (TimeRange range : queryRanges) {
+      if (range.duration() >= request.getDuration()) {
+        resultRanges.add(range);
+      }
+    }
+    Collections.sort(resultRanges, TimeRange.ORDER_BY_END);
+    return resultRanges;
+  }
+
+  private boolean hasOverlappingAttendees(Event event, MeetingRequest request,
+      Map<Event, Map<MeetingRequest, Boolean>> cache) {
+    Map<MeetingRequest, Boolean> overlappingRequests = cache.get(event);
+    if (overlappingRequests == null) {
+      overlappingRequests = new HashMap<>();
+      cache.put(event, overlappingRequests);
+    }
+    Boolean isOverlapping = overlappingRequests.get(request);
+    if (isOverlapping == null) {
+      isOverlapping = hasOverlappingAttendees(event, request);
+      overlappingRequests.put(request, isOverlapping);
+    }
+    return isOverlapping;
+  }
+
+  private boolean hasOverlappingAttendees(Event event, MeetingRequest request) {
+    Set<String> eventAttendees = new HashSet<>(event.getAttendees());
+    Set<String> reqAttendees = new HashSet<>(request.getAttendees());
+    for (String attendee : reqAttendees) {
+      if (eventAttendees.contains(attendee)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Removes any time from @range where @eventTime overlaps it.
+   * 
+   * @param range     The TimeRange from which available time will be selected.
+   * @param eventTime The TimeRange that is considered to be occupied.
+   * @return All TimeRange from @range where @eventTime is not occurring
+   */
+  private Collection<TimeRange> splitToAvailableTimeRanges(TimeRange range, TimeRange eventTime) {
+    Collection<TimeRange> availableRanges = new ArrayList<>();
+    if (!eventTime.overlaps(range)) {
+      availableRanges.add(range);
+      return availableRanges;
+    }
+    if (eventTime.contains(range)) {
+      return availableRanges;
+    }
+
+    TimeRange availableRange;
+    if (eventTime.end() > range.start() || range.end() > eventTime.end()) {
+      availableRange = TimeRange.fromStartEnd(eventTime.end(), range.end(), false);
+      availableRanges.add(availableRange);
+    }
+    if (range.end() > eventTime.start()) {
+      availableRange = TimeRange.fromStartEnd(range.start(), eventTime.start(), false);
+      availableRanges.add(availableRange);
+    }
+    return availableRanges;
   }
 }
